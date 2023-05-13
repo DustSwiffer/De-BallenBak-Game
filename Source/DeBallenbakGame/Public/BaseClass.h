bdcd34fbd1f361bd7d50579e18aae6d2e22e229b
DustSwiffer <dvh_1996@hotmail.com>
2023-05-13 18:26:59 +0200

    porten to c++ project

---
+++ Source/DeBallenbakGame/Public/BaseClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class DEBALLENBAKGAME_API BaseClass
+{
+public:
+	BaseClass();
+	~BaseClass();
+};
